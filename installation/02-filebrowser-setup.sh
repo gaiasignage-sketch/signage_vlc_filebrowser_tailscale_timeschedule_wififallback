91f45e305d88da136167a0c074629c8bab0300f0
gaiasignage-sketch <gaiasignage@gmail.com>
2026-05-12 19:32:16 +0200

    Update 02-filebrowser-setup.sh

--- installation/02-filebrowser-setup.sh
+++ installation/02-filebrowser-setup.sh
@@ -17,10 +17,10 @@ touch /home/rpi02w/filebrowser/filebrowser.db
 
 echo "FileBrowser setup complete!"
 echo "Next: Copy systemd/filebrowser.service to /etc/systemd/system/"
-sudo cp systemd/filebrowser.service /etc/systemd/system/"
-sudo systemctl daemon-reload"
-sudo systemctl enable filebrowser.service"
-sudo systemctl start filebrowser.service"
+sudo cp systemd/filebrowser.service /etc/systemd/system/
+sudo systemctl daemon-reload
+sudo systemctl enable filebrowser.service
+sudo systemctl start filebrowser.service
 echo "Access FileBrowser at: http://<rpi-ip>:8080"
 echo "Default password: gaia12345678"
 
