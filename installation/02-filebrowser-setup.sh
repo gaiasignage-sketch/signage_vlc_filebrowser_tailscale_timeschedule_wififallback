5ed61ab8056a7fb3a6aaad385a82b5f17857bf47
gaiasignage-sketch <gaiasignage@gmail.com>
2026-05-13 01:46:20 +0200

    Update 02-filebrowser-setup.sh

--- installation/02-filebrowser-setup.sh
+++ installation/02-filebrowser-setup.sh
@@ -12,9 +12,9 @@ echo "Installing FileBrowser..."
 curl -fsSL https://raw.githubusercontent.com/filebrowser/get/master/get.sh | bash
 
 echo "Creating FileBrowser directory..."
-sudo touch /home/rpi02w/filebrowser.db
-sudo filebrowser config init -d /home/rpi02w/filebrowser.db -r /home/rpi02w/autoplay
-sudo filebrowser --password gaia12345678 -d /home/rpi02w/filebrowser.db   -r /home/rpi02w/autoplay   -a 0.0.0.0   -p 8080 
+touch /home/rpi02w/filebrowser.db
+filebrowser config init -d /home/rpi02w/filebrowser.db -r /home/rpi02w/autoplay
+filebrowser --password gaia12345678 -d /home/rpi02w/filebrowser.db   -r /home/rpi02w/autoplay   -a 0.0.0.0   -p 8080 
 echo "FileBrowser setup complete!"
 echo "Next: Copy systemd/filebrowser.service to /etc/systemd/system/"
 sudo cp systemd/filebrowser.service /etc/systemd/system/
