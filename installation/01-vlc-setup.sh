a8e53028796518fe9f0884b1f8e0afc714cb44c1
gaiasignage-sketch <gaiasignage@gmail.com>
2026-05-12 19:31:44 +0200

    Update 01-vlc-setup.sh

--- installation/01-vlc-setup.sh
+++ installation/01-vlc-setup.sh
@@ -17,7 +17,7 @@ sudo chmod -R 755 /home/rpi02w/autoplay
 
 echo "VLC setup complete!"
 echo "Next: Copy systemd/vlc-autoplay.service to /etc/systemd/system/"
-sudo cp systemd/vlc-autoplay.service /etc/systemd/system/"
-sudo systemctl daemon-reload"
-sudo systemctl enable vlc-autoplay.service"
-sudo systemctl start vlc-autoplay.service"
+sudo cp systemd/vlc-autoplay.service /etc/systemd/system/
+sudo systemctl daemon-reload
+sudo systemctl enable vlc-autoplay.service
+sudo systemctl start vlc-autoplay.service
